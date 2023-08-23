4d9ace3ae9a1a2b8ee732c0b0157663240c0d82d
Zachary Vorhies <zachvorhies@protonmail.com>
2023-08-22 18:27:53 -0700

    Update install_idf.bat

--- install_idf.bat
+++ install_idf.bat
@@ -7,7 +7,7 @@ set idf_install_url=https://dl.espressif.com/dl/idf-installer/%idf_install_exe%
 
 REM prompt if user wants to install %idf_install_exe%
 echo %idf_install_exe% is required to install the ESP32 toolchain.
-set /p install_idf=Do you want to install it now (y/n)?
+set /p install_idf=Do you want to install it now (y/n)? 
 if %install_idf%==y (
     powershell -Command "Invoke-WebRequest -Uri %idf_install_url% -OutFile %idf_install_exe%"
     cmd /c %idf_install_exe%
