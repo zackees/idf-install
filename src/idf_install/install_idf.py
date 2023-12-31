75011f72f3cb52e98b0e0935e16249f2d7002c1b
zackees <z@zackees.com>
2023-12-30 16:02:37 -0800

    update

--- src/idf_install/install_idf.py
+++ src/idf_install/install_idf.py
@@ -4,13 +4,36 @@ import os
 import shutil
 import subprocess
 import sys
+import argparse
 
 # Set environment variables
 IDF_VER = "v5.0"
 IDF_PATH = f"./esp-idf/{IDF_VER}"
 IDF_TARGETS = "esp32,esp32s3,esp32c3"
 
+
+
 def main() -> int:
+    parser = argparse.ArgumentParser(    
+        description="Install ESP-IDF toolchain",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    # --non-interactive
+    parser.add_argument(
+        "--non-interactive",
+        help="Do not ask for confirmation before removing the directory",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    def ask_user(msg: str) -> bool:
+        if args.non_interactive:
+            return True
+        else:
+            choice = input(msg).lower()
+            if choice == "y":
+                return True
+            else:
+                return False
     # Get the python path
     python_path = shutil.which("python")
     assert python_path is not None, "Python not found in PATH"
@@ -27,8 +50,8 @@ def main() -> int:
         print(
             f"Warning: This script will remove the {IDF_PATH} directory and all its contents."
         )
-        choice = input("Continue? [y/n]: ").lower()
-        if choice == "y":
+        do_continue = ask_user("Continue? [y/n]: ")
+        if do_continue:
             print(f"Removing {IDF_PATH} directory")
         else:
             print("Aborting")
