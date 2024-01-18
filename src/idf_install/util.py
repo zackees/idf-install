87236ca2c9d293e2cb7f7a65d0b978833ae3542a
zackees <z@zackees.com>
2024-01-17 17:23:29 -0800

    try to re-write safe_rmtree for windows

--- src/idf_install/util.py
+++ src/idf_install/util.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import stat
 from warnings import warn
 
 from send2trash import send2trash
@@ -11,44 +10,21 @@ def touch_file(filepath: str) -> None:
         pass
 
 
-def onerror(func, path, exc_info):  # pylint: disable=unused-argument
-    """
-    Error handler for ``shutil.rmtree``.
+def safe_rmtree(path: str) -> None:
+    """Delete a directory, but send it to the trash if it fails."""
 
-    If the error is due to an access error (read only file)
-    it attempts to add write permission and then retries.
+    is_windows = os.name == "nt"
+    if is_windows:
+        os.system(f'rmdir /S /Q "{path}"')
+    else:
+        os.system(f'rm -rf "{path}"')
 
-    If the error is for another reason it re-raises the error.
-
-    Usage : ``shutil.rmtree(path, onerror=onerror)``
-    """
     if not os.path.exists(path):
         return
 
-    # Is the error an access error?
-    if not os.access(path, os.W_OK):
-        try:
-            os.chmod(path, stat.S_IWUSR)
-            func(path)
-        except PermissionError:
-            return
-    else:
-        return
-
-
-def safe_rmtree(path: str) -> None:
-    """Delete a directory, but send it to the trash if it fails."""
     shutil.rmtree(path, ignore_errors=True)
     if not os.path.exists(path):
         return
-
-    shutil.rmtree(path, onerror=onerror)
-    if not os.path.exists(path):
-        return
-    if os.path.exists(path):
-        is_windows = os.name == "nt"
-        if is_windows:
-            os.system(f'rmdir /S /Q "{path}"')
     if os.path.exists(path):
         warn(
             f"Could not fully remove {path} using shutil.rmtree,"
